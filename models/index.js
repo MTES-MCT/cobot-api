cc2e03cf892d45c13541f4997700c12c2945d5f1
Stéphane legouffe <slegouffe@gmail.com>
2018-03-21 16:10:58 +0100

    CC-42: Créer l'API de gestion des messages

--- models/index.js
+++ models/index.js
@@ -3,10 +3,14 @@ import config from '../config';
 import Logger from '../utils/logger';
 
 import Users from './users';
+import * as Messages from './messages';
 
+mongoose.set('debug', false);
 mongoose.connect(`mongodb://${config.database.host}:${config.database.port}/${config.database.name}`).then(
   () => {
     exports.Users = Users;
+    exports.Messages = Messages.Message;
+    exports.Attachments = Messages.Attachments;
     Logger.log('info', 'Connected to database "%s"', config.database.name);
   },
   (err) => { throw new Error('Unable to connect to mongodb', err); },
